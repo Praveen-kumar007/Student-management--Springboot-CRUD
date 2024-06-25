46fd31d5ecbb6985b9b8329ea0e1d72e318f91cf
Praveen-kumar007 <153615619+Praveen-kumar007@users.noreply.github.com>
2024-06-25 20:26:17 +0530

    Update SpringBootRestApiProjectApplication.java

--- SpringBootRestAPIProject/src/main/java/com/praveen/restapi/SpringBootRestApiProjectApplication.java
+++ SpringBootRestAPIProject/src/main/java/com/praveen/restapi/SpringBootRestApiProjectApplication.java
@@ -1,4 +1,4 @@
-package com.preetha.restapi;
+package com.praveen.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
